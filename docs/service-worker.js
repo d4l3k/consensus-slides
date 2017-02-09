28c635fad34ecf174b7d6887a825704bf0c123a6
Tristan Rice <rice@fn.lc>
2017-02-08 22:58:56 -0800

    admin: added an individual indicator

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -29,7 +29,7 @@
 
 
 /* eslint-disable quotes, comma-spacing */
-var PrecacheConfig = [["/components/index-page.html","1148f895d7a5d775753cc95a34d43eec"],["/components/main-app.html","836c903d945a63b65fd57132be88ea4c"],["/components/presentation-page.html","16fb5582fdb899d2e07b2ebb91799020"],["/index.html","3e3f24daafbd98f56ae1a7cf2283ddcd"]];
+var PrecacheConfig = [["/components/index-page.html","a28d22dec4c37dcfd39d31d99b4f5c5e"],["/components/main-app.html","836c903d945a63b65fd57132be88ea4c"],["/components/presentation-page.html","cdf4c03b3734fd990e67be1d64654442"],["/index.html","b9d5fe524efd7c94b08a68ae1cd83dc5"]];
 /* eslint-enable quotes, comma-spacing */
 var CacheNamePrefix = 'sw-precache-v1--' + (self.registration ? self.registration.scope : '') + '-';
 
